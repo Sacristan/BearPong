027078b5aadb7bb3cdad79f8ea9f89176e67c40a
Sacristan <girts.kesteris@gmail.com>
2017-04-30 11:37:20 +0300

    added spectator behaviour

---
+++ Assets/SpectatorBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorBehavior : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 localOffset = Vector3.up;
+
+    [SerializeField]
+    private float durationInSeconds = 3f;
+
+    [SerializeField]
+    private float allowMovementDelayTime = 0f;
+
+    private Vector3 _originalPos;
+
+    float perc = 0f;
+
+    private bool allowMovement = false;
+
+    IEnumerator Start()
+    {
+        _originalPos = transform.localPosition;
+        yield return new WaitForSeconds(allowMovementDelayTime);
+        allowMovement = true;
+    }
+
+    void Update()
+    {
+        if (!allowMovement) return;
+        perc = Mathf.PingPong(Time.time / durationInSeconds, 1f);
+        transform.localPosition = Vector3.Lerp(_originalPos, _originalPos + localOffset, perc);
+    }
+}

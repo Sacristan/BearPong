8a6d3f796b1c152269265487edd8ad5f89b4c670
Zintis Erics <zintis.erics@outlook.com>
2017-04-30 05:07:08 +0300

    Trees.

---
+++ Assets/Scripts/GameLostBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class GameLostBehaviour : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartCoroutine(Blackout());
+    }
+
+    private IEnumerator Blackout()
+    {
+        VignetteAndChromaticAberration eff = GetComponentInParent<VignetteAndChromaticAberration>();
+        while (!(eff.intensity > float.Epsilon))
+        {
+            eff.intensity -= Time.deltaTime / 4;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
